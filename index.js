d635fde662a3f343d0bbfa20fb25c4ea49b33f85
Mathias Buus <mathiasbuus@gmail.com>
2017-06-21 11:07:06 +0200

    Fallback to js version from node (#2)
    * fallback to js version from node
    
    * do not include process stub in browser

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('sodium-native')
+try {
+  if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
+  module.exports = require('sodium-native')
+} catch (err) {
+  module.exports = require('sodium-javascript')
+}
