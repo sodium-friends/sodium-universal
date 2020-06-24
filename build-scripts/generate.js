328705a0bd4fe1d8f03d8ade2b40cdbc6a32c996
Emil Bay <github@tixz.dk>
2020-06-24 16:50:57 +0200

    Recurse into subdirs

--- build-scripts/generate.js
+++ build-scripts/generate.js
@@ -2,18 +2,26 @@
 
 const fs = require('fs')
 const path = require('path')
-const dir = path.dirname(require.resolve('sodium-javascript/package.json'))
+const root = path.dirname(require.resolve('sodium-javascript/package.json'))
 
-const ls = fs.readdirSync(dir)
-const tmp = `module.exports = require('sodium-native')\n`
+const tmp = "module.exports = require('sodium-native')\n"
 
-for (const file of ls) {
-  if (!/\.js$/i.test(file)) continue
-  if (file === 'example.js') continue
-  if (file === 'test.js') continue
+function recurse (dir) {
+  const ls = fs.readdirSync(dir)
+  const subdir = path.relative(root, dir)
+  if (subdir) fs.mkdirSync(subdir)
 
-  fs.writeFileSync(path.join(__dirname, '..', file), tmp.replace('{file}', file))
+  for (const file of ls) {
+    if (file === 'internal') recurse(path.join(dir, file))
+    if (!/\.js$/i.test(file)) continue
+    if (file === 'example.js') continue
+    if (file === 'test.js') continue
+
+    fs.writeFileSync(path.join(__dirname, '..', subdir, file), tmp)
+  }
 }
+
+recurse(root)
 
 const pkg = require('sodium-javascript/package.json')
 const myPkg = require('../package.json')
